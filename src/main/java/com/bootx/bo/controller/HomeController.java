b6c60351cfbebab7903024921d66f71adddd63be
marvin.min <marvin.min@activenetwork.com>
2021-07-27 15:06:20 +0800

    hello

---
+++ src/main/java/com/bootx/bo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.bootx.bo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public Object home(){
+        return "Hello";
+    }
+}
